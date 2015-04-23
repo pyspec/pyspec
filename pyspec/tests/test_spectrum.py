601832adc15b3347dd810e05ccd5da715b69ff26
Cesar B Rocha <crocha@ucsd.edu>
2015-04-22 17:54:13 -0700

    added test to 2d parsevals relation; its working with zero mean

--- pyspec/tests/test_spectrum.py
+++ pyspec/tests/test_spectrum.py
@@ -1,7 +1,7 @@
 import numpy as np
 from pyspec import spectrum as spec
 
-def test_parseval(rtol=1.e-14, debug=False):
+def test_parseval_1d(rtol=1.e-14, debug=False):
     """ Make sure 1D spectrum satisfied Parseval relation """
  
     N = np.array([10,11,21,500,871,1000,1001])
@@ -30,7 +30,40 @@ def test_parseval(rtol=1.e-14, debug=False):
                 print "error = %5.16f" %error
                 print " "
 
-            assert error<rtol, " *** Does not satisfy Parseval's relation "
+            assert error<rtol, " *** 1D spectrum Does not satisfy Parseval's relation "
+
+def test_parseval_2d(rtol=1.e-14, debug=False):
+    """ Make sure 2D spectrum satisfied Parseval relation """
+ 
+    N = np.array([10,11,21,500,871,1000,1001])
+    dta = np.array([.25,1.,1.67,6.,20.])
+
+    for dt in dta: 
+
+        for n in N:
+        
+            phi = np.random.randn(n,n)
+            phi = phi - phi.mean()
+            spec_phi = spec.TWODimensional_spec(phi,dt,dt)
+
+            # var(P) from Fourier coefficients
+            P_var_spec = spec_phi.var 
+
+            # var(P) in physical space
+            P_var_phys = phi.var()
+
+            # relative error
+            error = np.abs(P_var_phys - P_var_spec)/P_var_phys
+
+            if debug:
+                print "N = %i,  dt = %2.5f" %(n,dt)
+                print "Variance in physical space: %5.16f" %P_var_phys
+                print "Variance from spectrum: %5.16f" %P_var_spec
+                print "error = %5.16f" %error
+                print " "
+
+            assert error<rtol, " *** 2D spectrum Does not satisfy Parseval's relation "
+
 
 if __name__ == "__main__":
-    test_parseval(debug=True)
+    test_parseval_2d(debug=True)
